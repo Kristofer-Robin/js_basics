be66ad4c48e3b3f87484491885ff435a32a65e81
Kristofer-Robin <kristofer-robin.lai@voco.ee>
2022-10-05 14:38:19 +0300

    Õuntejagamine 3.4

--- varibles/apps.js
+++ varibles/apps.js
@@ -1,7 +1,11 @@
 // kasutaja sisend
-let taringuteArv = Number(prompt('Täringute arv'))
-
-for(let kord = 1; kord <= taringuteArv; kord++){
-    let taring = Math.random() * 6
-    console.log(taringud)
+let poialpoisid = number(prompt('Mitu pöialpoisi tahab õunu'))
+let lumivalgeke0unad = 14
+let poialpoiss = 1
+while(poialpoiss <= poialpoisid){
+    let ounad = Math.ceil(Math.random() * 2)
+    console.log(ounad)
+    lumivalgeke0unad = lumivalgeke0unad - ounad
+    poialpoiss++
 }
+console.log(`Lumivalgekesel jäi ${lumivalgeke0unad}`)
