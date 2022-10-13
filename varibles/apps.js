645c17f4d7abd28402636499bda696bf24b32b3a
Kristofer-Robin <kristofer-robin.lai@voco.ee>
2022-10-13 12:04:16 +0300

    Pulss 2.3

--- varibles/apps.js
+++ varibles/apps.js
@@ -1,22 +1,46 @@
-let valik = Number(prompt('Kas soovid istekohta ise valida +
-"ise" või loosida "loos"'))
-
-if(valik == 'ise'){
-    let kohtValik = prompt('Kas soovite istuda akna ääres +
-    "aken" või muu "muu"')
-    console.log('Valisite ise.')
-    if(kohaValik == 'aken'){
-        koht = 'Aknakoht.'
-    } else if(kohaValik == 'muu'){
-        koht = 'Vahekäigukoht.'
-    }
-} else if(valik == 'loos'){
-    console.log('Istekoht loositi.')
-    let juhuarv = Math.ceil(Math.random() * 3)
-    if(juhuarv == 1) {
-        koht = 'Aknakoht.'
-    } else {
-        koht = 'Vahekäigukoht.'
-    }
+kui aga mitte, vaadatakse järgmine tingimus
+} else {
+    täidetakse siis, kui kõik eelmised ei sobinud
 }
-console.log(koht)
+*/
+
+/*
+if(tingimus1){
+    if(tingimus)
+ */
+let vanus = Number(prompt('Sisestage oma vanus'))
+let sugu = prompt('Sisestage oma sugu')
+let treening = Number(prompt('Sisestage treeningu tüüp'))
+// kontroll ja väljastus
+let plusisagedus;
+
+if(sugu == 'm' || sugu == 'M'){
+    {console.log('mees')
+    } else if(sugu == 'n' || sugu == 'N'){
+        console.log('naine')
+    }
+    if(sugu == 'm' || sugu == 'M'){
+        pulsisagedus = 220 - vanus
+    } else if(sugu == 'n' || sugu == 'N'){
+        pulsisagedus = 206 - 0.88 * vanus
+    }
+    let minPulsisagedus;
+    let maxPulsisagedus;
+
+    if(treening == 1){
+        minPulsisagedus = 0.5 * pulsisagedus
+        maxPulsisagedus = 0.7 * pulsisagedus
+    } else if(treening == 2){
+        minPulsisagedus = 0.7 * pulsisagedus
+        maxPulsisagedus = 0.8 * pulsisagedus
+    } else if(treening == 3){
+        minPulsisagedus = 0.8 * pulsisagedus
+        maxPulsisagedus = 0.87 * pulsisagedus
+    }
+/*
+console.log('Pulsisagedus peab olema vahemikus ' +
+Math.round (minPulsisagedus) + " kuni " + Math.round (maxPulsisagedus))
+
+    console.log('Pulsisagedus peab olema vahemikus ${minPulsisagedus} kuni ${maxPulsisagedus}')
+    See on lihtsam viis lahetada seda ül
+ */
