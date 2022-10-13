21a9f53f0ca72f6f09bb0d5affa39d8bc52e1ac6
Kristofer-Robin <kristofer-robin.lai@voco.ee>
2022-10-13 12:04:38 +0300

    Transport 1.6

--- varibles/apps.js
+++ varibles/apps.js
@@ -1,46 +1,52 @@
-kui aga mitte, vaadatakse järgmine tingimus
-} else {
-    täidetakse siis, kui kõik eelmised ei sobinud
-}
-*/
-
-/*
-if(tingimus1){
-    if(tingimus)
+/**
+ * documentation
+ * comment
+ * style
+ *
+ * variables - avr, let, const
+ *
+ * let variableName = value // mutable
+ * const constName = value // immutable
+ *
  */
-let vanus = Number(prompt('Sisestage oma vanus'))
-let sugu = prompt('Sisestage oma sugu')
-let treening = Number(prompt('Sisestage treeningu tüüp'))
-// kontroll ja väljastus
-let plusisagedus;
 
-if(sugu == 'm' || sugu == 'M'){
-    {console.log('mees')
-    } else if(sugu == 'n' || sugu == 'N'){
-        console.log('naine')
-    }
-    if(sugu == 'm' || sugu == 'M'){
-        pulsisagedus = 220 - vanus
-    } else if(sugu == 'n' || sugu == 'N'){
-        pulsisagedus = 206 - 0.88 * vanus
-    }
-    let minPulsisagedus;
-    let maxPulsisagedus;
+// this is simple comment
 
-    if(treening == 1){
-        minPulsisagedus = 0.5 * pulsisagedus
-        maxPulsisagedus = 0.7 * pulsisagedus
-    } else if(treening == 2){
-        minPulsisagedus = 0.7 * pulsisagedus
-        maxPulsisagedus = 0.8 * pulsisagedus
-    } else if(treening == 3){
-        minPulsisagedus = 0.8 * pulsisagedus
-        maxPulsisagedus = 0.87 * pulsisagedus
-    }
 /*
-console.log('Pulsisagedus peab olema vahemikus ' +
-Math.round (minPulsisagedus) + " kuni " + Math.round (maxPulsisagedus))
+console.log("first row")
+console.log("second row")
+console.log("third row") */
 
-    console.log('Pulsisagedus peab olema vahemikus ${minPulsisagedus} kuni ${maxPulsisagedus}')
-    See on lihtsam viis lahetada seda ül
+let numberOne = 5
+let numberTwo = 2.5
+const season = "Autumn"
+// season ) "Spring" ! error
+console.log(numberOne)
+console.log(numberTwo)
+console.log(season + season)
+
+
+// kasutaja sisend
+let inimesed = 100
+let kohtiBussis = 40
+
+// arvutus
+let bussidTäidetud = Math.floor(imimesed / kohtiBussis)
+let mahaJaanud = inimesed % kohtiBussis
+// tulemuse väljastamine
+console.log(inimesed + ' inimeste sõidutamiseks läheb välja ' + bussidTäidetud + ' bussi ')
+if(mahaJaanud != 0) {
+    console('ja ' + mahaJaanud + 'inimest on jäänud maha')
+}
+
+// == != > < >= <= - võrdlusoperaatorid -> true / false
+
+/*
+if(tingimus){
+   tegevused, mis sooritatakse, kui
+   vastus tingimusele on true
+} else {
+    tegevused, mis sooritatakse, kui
+    vastus tingimusele on fasle
+}
  */
