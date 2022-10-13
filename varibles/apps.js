0526c0b0eebc1ec62d7a84f883029c2584af139b
Kristofer-Robin <kristofer-robin.lai@voco.ee>
2022-10-13 12:02:34 +0300

    Jäätumine 2.1

--- varibles/apps.js
+++ varibles/apps.js
@@ -1,11 +1,8 @@
 // kasutaja sisend
-let poialpoisid = number(prompt('Mitu pöialpoisi tahab õunu'))
-let lumivalgeke0unad = 14
-let poialpoiss = 1
-while(poialpoiss <= poialpoisid){
-    let ounad = Math.ceil(Math.random() * 2)
-    console.log(ounad)
-    lumivalgeke0unad = lumivalgeke0unad - ounad
-    poialpoiss++
+let temperatuur = Number(prompt('Sisesta õhutemperatuur'))
+// kontroll ja väljastus
+if(temp > 4.0){
+    console.log('Ei ole jäätumise ohtu')
+} else {
+    console.log('On jäätumise oht')
 }
-console.log(`Lumivalgekesel jäi ${lumivalgeke0unad}`)
