0f41f8b20a2d0cfa372a237486d3be241575e18f
Kristofer-Robin <kristofer-robin.lai@voco.ee>
2022-10-13 12:02:28 +0300

    Jäätumine 2.1

---
+++ varibles/jää.js
@@ -0,0 +1,8 @@
+// kasutaja sisend
+let temperatuur = Number(prompt('Sisesta õhutemperatuur'))
+// kontroll ja väljastus
+if(temp > 4.0){
+    console.log('Ei ole jäätumise ohtu')
+} else {
+    console.log('On jäätumise oht')
+}
