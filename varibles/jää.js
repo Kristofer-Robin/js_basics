e1857e040338463a0d13223e114c2c727187c55b
Kristofer-Robin <112863303+Kristofer-Robin@users.noreply.github.com>
2022-10-17 14:27:49 +0300

    Delete jää.js

--- varibles/jää.js
+++
@@ -1,8 +0,0 @@
-// kasutaja sisend
-let temperatuur = Number(prompt('Sisesta õhutemperatuur'))
-// kontroll ja väljastus
-if(temp > 4.0){
-    console.log('Ei ole jäätumise ohtu')
-} else {
-    console.log('On jäätumise oht')
-}
